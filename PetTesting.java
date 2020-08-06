827392da2421748817b8db362ea071c86e40d022
SimoneJohnson04 <simonejohnson22@solonschools.net>
2020-08-06 17:40:51 +0000

    Finalized the program

--- PetTesting.java
+++ PetTesting.java
@@ -23,7 +23,7 @@ public class RabbitRunner {
     boolean gameOn = true;
     while(gameOn){
       System.out.printf("\n");
-      System.out.println(bunny.name + " eagerly awaits your attention. Select an action: \n1. Display your bunny's stats\n2. Feed the bunny some carrots(Weight + 0.2*carrots, Happiness + 1)\n3. Give the bunny some water(Weight + 0.2)\n4. Let your bunny hop through hoops(Energy-hops*0.1, Weight-hops*0.01)\n5. Cuddle with the bunny(Happiness + 1)\n6. Let the bunny take a nap(Energy + 0.01*Minutes)\n7. Teach your bunny a new trick(Intelligence + 1, Energy - 1)\n0. Stop playing with the virtual bunny");
+      System.out.println(bunny.name + " eagerly awaits your attention. Select an action: \n1. Display your bunny's stats\n2. Feed the bunny some carrots(Weight + 0.2*carrots, Happiness + 1)\n3. Give the bunny some water(Weight + 0.2)\n4. Let your bunny hop through hoops(Energy-hops*0.1, Weight-hops*0.05)\n5. Cuddle with the bunny(Happiness + 1)\n6. Let the bunny take a nap(Energy + 0.01*Minutes)\n7. Teach your bunny a new trick(Intelligence + 1, Energy - 1)\n0. Stop playing with the virtual bunny");
       choice = keyboard.nextInt();
       System.out.printf("\n");
 
@@ -131,59 +131,59 @@ public class Pet{
   public void eat(int carrots){
     weight = weight + carrots*.2;
     happiness=happiness+1;
-    System.out.printf("Your bunny ate %d carrots. It now weighs %.1f pounds and has a happiness of %d\n", carrots, weight, happiness);
+    System.out.printf("%s ate %d carrots. It now weighs %.1f pounds and has a happiness of %d\n", name, carrots, weight, happiness);
   }
 
   //allows the bunny to drink water (and gain minimal weight)
   public void drink(){
     weight = weight + .2;
-    System.out.printf("Your bunny drank some water. It now weighs %.1f pounds\n", weight);
+    System.out.printf("%s drank some water. It now weighs %.1f pounds\n", name, weight);
   }
 
   //has the bunny hop the amount of times the user inputs, and has the bunny lose the porportional amount of weight and energy
   public void hop(int hops){
     energy = energy-(hops*0.1);
     weight = weight - (hops*0.05);
-    System.out.printf("Your bunny hopped %d times. Your rabbit now weighs %.1f pounds and has %.1f energy.\n", hops, weight,energy);
+    System.out.printf("%s hopped %d times. Your rabbit now weighs %.1f pounds and has %.1f energy.\n", name, hops, weight,energy);
   }
 
   //allows the bunny to cuddle with the user, and makes the bunny happy
   public void cuddle(){
     happiness = happiness + 1;
-    System.out.printf("Your bunny cuddled with you. Your rabbit now has a happiness of %d\n", happiness);
+    System.out.printf("%s cuddled with you. Your rabbit now has a happiness of %d\n", name, happiness);
   }
 
   //has the bunny sleep for a while in order to get more energy
   public void nap(int minutes){
     energy = energy+(minutes*.01);
-    System.out.printf("Your bunny slept for %d minutes. Your rabbit now has %.1f energy.\n",minutes, energy);
+    System.out.printf("%s slept for %d minutes. Your rabbit now has %.1f energy.\n", name, minutes, energy);
   }
   
   //has the bunny learn a new trick and gain intelligence, but also takes a lot of energy
   public void trick(){
     intelligence++;
     energy = energy - 1;
-    System.out.printf("Your bunny learned a new trick and now has an intelligence of %d and %.1f energy\n", intelligence, energy);
+    System.out.printf("%s learned a new trick and now has an intelligence of %d and %.1f energy\n", name, intelligence, energy);
   }
 
   //method that gives feedback to the player if the bunny is extremely overweight, underweight, tired, really energetic, or very sad. If any of these get too high, then the rabbit is removed and the game is over by returning a boolean value of false to the GameOn boolean in the PetTesting class.
   public boolean bunnyState(){
     double changeWeight = weight-storeWeight;
-    if(changeWeight>=2 && changeWeight<4){
+    if(changeWeight>=2 && changeWeight<5){
       System.out.printf("Your bunny is quite chubby and your mom threatens to give it away if you don't take care of it.\n");
       happiness--;
     }
-    if(changeWeight>=4){
+    if(changeWeight>=5){
       System.out.printf("Your bunny is obese and your mom gives it to someone who will take care of it right.\n");
       happiness--;
       happiness--;
       return false;
     }
-    if(changeWeight<=-2 && changeWeight>-4){
+    if(changeWeight<=-2 && changeWeight>-5){
       System.out.printf("Your bunny is quite thin and your mom threatens to give it away if you don't take care of it.\n");
       happiness--;
     }
-    if(changeWeight<=-4){
+    if(changeWeight<=-5){
       System.out.printf("Your bunny is underweight and your mom gives it to someone who will take care of it right.\n");
       happiness--;
       happiness--;
@@ -204,7 +204,6 @@ public class Pet{
     }
     if(happiness<5 && happiness>3){
       System.out.printf("Your bunny is unhappy and your mom threatens to take it away.");
-      happiness--;
     }
     if(happiness<=3){
       System.out.printf("Your bunny is very unhappy  and your mom gives it away to someone who will take care of it right.\n");
